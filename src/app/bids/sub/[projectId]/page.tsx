dbf6cbdfceb8887a1fa3556b9d3d175c0d091099
Austin <austinemshoff@gmail.com>
2026-02-16 18:04:57 -0700

    Revert "Update bids UI"
    This reverts commit 1cc92317086d2a4200e25cc9adb5e0e667b1ba03.

--- src/app/bids/sub/[projectId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import SubBidWorkspace from "@/components/bids/sub-workspace";
-
-export default async function SubBidWorkspacePage({ params }: { params: Promise<{ projectId: string }> }) {
-  const { projectId } = await params;
-  return <SubBidWorkspace projectId={projectId} />;
-}
