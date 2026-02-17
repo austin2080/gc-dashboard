f038cd0d920c837151388cad965ea24e6393a82a
Austin <austinemshoff@gmail.com>
2026-02-16 18:11:22 -0700

    Revert "Move bid actions into main tabs"
    This reverts commit ccc3fe0110bd74e3b236c1bccd366fbd5e0d3a47.

--- src/app/(bid-management)/bidding/templates/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function BidTemplatesPage() {
-  return (
-    <main className="bg-slate-50 p-4 sm:p-6">
-      <div className="rounded-2xl border border-dashed border-slate-200 bg-white p-8 text-center text-sm text-slate-500">
-        Templates content coming soon.
-      </div>
-    </main>
-  );
-}
